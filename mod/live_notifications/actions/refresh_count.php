8e5d5085491b85a2e0448f7c2fe50e84fb45f285
Abraham Aguilar <zig.jhos@gmail.com>
2014-10-03 13:54:03 -0500

    Fix notification error

--- mod/live_notifications/actions/refresh_count.php
+++ mod/live_notifications/actions/refresh_count.php
@@ -1,6 +1,10 @@
-<?php 
-gatekeeper();
+<?php
+if (elgg_is_logged_in()) {
+    gatekeeper();
 
-$num_messages = count_unread_notifications(25);
+    $num_messages = count_unread_notifications(25);
 
-echo $num_messages;
+    echo $num_messages;
+}else{
+    gatekeeper();
+}
