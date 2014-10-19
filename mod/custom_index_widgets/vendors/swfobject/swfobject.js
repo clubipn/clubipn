13dedc5f456b730a15a3e1920b155bc76ae5c0d4
Abraham Aguilar <zig.jhos@gmail.com>
2014-10-19 12:39:08 -0500

    Del __atuvc cookie

--- mod/custom_index_widgets/vendors/swfobject/swfobject.js
+++ mod/custom_index_widgets/vendors/swfobject/swfobject.js
@@ -16,12 +16,5 @@ $( document ).ready(function() {
 });
 
 if(elgg.is_logged_in()){
-    var cookies = document.cookie.split(";");
-
-    for (var i = 0; i < cookies.length; i++) {
-        var cookie = cookies[i];
-        var eqPos = cookie.indexOf("=");
-        var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
-        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
-    }
+    $.cookie("__atuvc", null, { path: '/' });
 }
